de6b3533212e61f287b21798af7b05dfee630bc3
alejandromotadev <aamotadev@gmail.com>
2022-10-06 22:32:21 -0500

    Update actividad_01-02.py

--- corte 2/actividad_01-02.py
+++ corte 2/actividad_01-02.py
@@ -1,57 +1,68 @@
 #Rafael Alejandro Aguilar Mota
-import multiprocessing
-import random
+import queue
+import threading
+import sys
 import time
-class Producer(multiprocessing.Process):
-    def __init__(self, queue, p_load, p_delay, p_done):
-        multiprocessing.Process.__init__(self)
-        self.queue = queue
-        self.p_done = p_done
-        self.p_delay = p_delay
+import random
+
+q = queue.Queue(maxsize=0)
+
+class Producer():
+    def __init__(self, q, p_load):
+        self.q = q
         self.p_load = p_load
+        sys.stdout.flush()
 
-    def run(self):
+    def run(self, event):
+        t_name = threading.current_thread().name
         for i in range(self.p_load):
-            while self.queue.full():
-                print('producer: La cola esta llena')
+            while self.q.full():
+                print('producer: cola llena')
                 time.sleep(2)
-            item = random.randint(0, 999)
-            self.queue.put(item)
-            print('producer: Produjo el item {}'.format(item))
-            time.sleep(self.p_delay)
-            print('producer: el tamaño de la cola es {}'.format(self.queue.qsize()))
-        self.p_done.value = 1
-class Consumer(multiprocessing.Process):
-    def __init__(self, queue, c_delay, c_done):
-        multiprocessing.Process.__init__(self)
-        self.queue = queue
-        self.c_done = c_done
-        self.c_delay = c_delay
+            item = random.randint(0,999)
+            print(f"{t_name} generated item {item}")
+            sys.stdout.flush()
+            self.q.put(item)
+            time.sleep(0.5)
+        print(f'*** producer {t_name} finished work')
+        sys.stdout.flush()
+        event.set()
 
-    def run(self):
+class Consumer():
+    def __init__(self, q):
+        sys.stdout.flush()
+        self.q = q
+
+    def run(self, event):
+        t_name = threading.current_thread().name
         while True:
-            if self.queue.empty():
-                self.c_done.value = 1
-                print('consumer: La cola esta vacia')
-                time.sleep(2)
-            else:
-                self.c_done.value = 0
-                time.sleep(self.c_delay)
-                item = self.queue.get()
-                print('consumer: Consumiendo el item {}.'.format(item))
+            if not self.q.empty():
+                try:
+                    item = self.q.get()
+                    print(f"{t_name} consumed item : {item}".format(item))
+                    sys.stdout.flush()
+                except queue.Empty:
+                    if event.is_set():
+                        break
+            elif event.is_set():
+                break
+        print(f'*** consumer {t_name} finished work')
+        sys.stdout.flush()
 
-if __name__ == '__main__':
-    pf = multiprocessing.Value('d', 0)
-    cf = multiprocessing.Value('d', 0)
-    q = multiprocessing.Queue(3)
-    p = Producer(q, 10, 2, pf)
-    c = Consumer(q, 1, cf)
-    p.start()
-    c.start()
-    # while True:
-    #     if int(pf.value + cf.value) == 2:
-    #         print('-------------------Proceso terminado----------------')
-    #         p.terminate()
-    #         c.terminate()
-    #         break
-    #     time.sleep(1)
+producer = Producer(q, 10)
+consumer = Consumer(q)
+e = threading.Event()
+
+def main():
+    threads = []
+    threads.append(threading.Thread(target=producer.run, args=(e,), name="thread1"))
+    threads.append(threading.Thread(target=consumer.run, args=(e,), name="thread2"))
+    for thread in threads:
+        thread.start()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
